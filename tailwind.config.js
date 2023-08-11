3e49de34122c2079c43f7d24e893d21d1d110f5c
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-08-11 22:08:19 +0700

    Fix minor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "/src/index.html",
+    "./src/index.html",
     "./src/**/*.{html,js}"
   ],
   theme: {
@@ -9,4 +9,3 @@ module.exports = {
   },
   plugins: [],
 }
-
